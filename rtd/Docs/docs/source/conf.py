b8b36aab91afe46331252a59e3fa6fbf34079a73
paharel411-creator <paharel411@gmail.com>
2026-05-04 17:54:10 +0100

    Update conf.py

--- rtd/Docs/docs/source/conf.py
+++ rtd/Docs/docs/source/conf.py
@@ -1,11 +1,22 @@
-project = "PrimeScore"
-author = "PrimeScore Team"
-copyright = "2026, PrimeScore Team"
+import os
+import sys
 
-extensions = []
+
+sys.path.insert(0, os.path.abspath(".."))
+
+project = "PrimeScore"
+copyright = "2026, Group 7A"
+author = "Group 7A"
+release = "latest"
+
+extensions = [
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
-html_theme = "alabaster"
+html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
+html_title = "PrimeScore documentation"
