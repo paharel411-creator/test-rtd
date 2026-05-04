0dfb882aedbe672507b1b8513a4baef08a80e315
paharel411-creator <paharel411@gmail.com>
2026-05-04 18:28:39 +0100

    Update conf.py

--- rtd/Docs/docs/source/conf.py
+++ rtd/Docs/docs/source/conf.py
@@ -1,1 +1,27 @@
+project = 'PrimeScore'
+copyright = '2026, PrimeScore Development Team'
+author = 'PrimeScore Development Team'
 
+release = '1.0'
+version = '1.0.0'
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+html_theme = 'sphinx_rtd_theme'
+
+epub_show_urls = 'footnote'
